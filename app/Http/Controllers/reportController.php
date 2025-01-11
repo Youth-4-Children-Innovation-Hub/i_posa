bf318d9823b9751243b92be298b92c8a09e16703
mbalike <dmbalike@outlook.com>
2025-01-11 19:41:55 +0300

    trial one

--- app/Http/Controllers/reportController.php
+++ app/Http/Controllers/reportController.php
@@ -452,9 +452,13 @@ class reportController extends Controller
         public function centerCourses(){
 
             $center   = Center::select('centers.name AS name')->first();
-            $course   = CourseCenter::select('centers.name AS name')->first();
+            $course   = CourseCenter::select('course.name AS course', 'teachers.name AS teacher','course_center.id AS id')
+                                  ->leftjoin('teachers', 'teachers.id', '=', 'course_center.id')
+                                  ->leftjoin('courses', 'courses.id', '=', 'course_center.id')
+                                  ->get();
 
-
+               $pdf = Pdf::loadView('report.centerCoursePdf',['center' => $center, 'course' => $course]);
+               return $pdf->stream('center_course.pdf');
         }
 
 
